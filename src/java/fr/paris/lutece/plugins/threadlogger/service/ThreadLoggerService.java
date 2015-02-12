8f1eab2dfcb06d0bde42cd5f9af85b9852d09ed7
PLE <pierrelevy@users.noreply.github.com>
2015-02-12 18:40:53 +0100

    THREADLOG-1 : Thread id added and id of thread that hold the lock

--- src/java/fr/paris/lutece/plugins/threadlogger/service/ThreadLoggerService.java
+++ src/java/fr/paris/lutece/plugins/threadlogger/service/ThreadLoggerService.java
@@ -78,12 +78,12 @@ public class ThreadLoggerService
                 Thread.State state = threadInfo.getThreadState(  );
                 LockInfo lock = threadInfo.getLockInfo(  );
 
-                sbLog.append( "[Thread] [" ).append( state.name(  ) ).append( "] [" )
+                sbLog.append( "[Thread] [" ).append( threadInfo.getThreadId()).append("] [").append( state.name(  ) ).append( "] [" )
                      .append( threadInfo.getThreadName(  ) ).append( "]" );
 
                 if ( lock != null )
                 {
-                    sbLog.append( " locked on " ).append( lock.getClassName(  ) );
+                    sbLog.append( " locked on " ).append( lock.getClassName(  )).append( "by Thread ID:").append( threadInfo.getLockOwnerId());
                 }
 
                 sbLog.append( " - bc=" ).append( threadInfo.getBlockedCount(  ) );
